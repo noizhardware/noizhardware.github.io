e08efc430d68c701806fabb43c3f2a7f886f0019
noizhardware <noisefuel@gmail.com>
2019-06-06 21:40:32 +0200

    fixed string to INT

--- tt/tt.js
+++ tt/tt.js
@@ -1,14 +1,17 @@
 var TT = {
   full: function(){
-    var seconds = Math.round(new Date() / 1000);
+    //var seconds = Math.round(new Date() / 1000);
     //document.getElementById("clock").innerHTML = `MASS element: "${fileArray[block][element][zeroOne]}"`;
-    var h = moment().format('HH');
-    var m = moment().format('mm');
-    var s = moment().format('ss');
+    var hs = moment().format('HH');
+    var h = parseInt(hs, 10);
+    var ms = moment().format('mm');
+    var m = parseInt(ms, 10);
+    var ss = moment().format('ss');
+    var s = parseInt(ss, 10);
     var totSec = (h * 3600) + (m * 60) + s;
     if (totSec <= 28800){ttMark = "S";}
-    if ((totSec > 28800) && (totSec < 57600)){ttMark = "M";}
-    if (totSec >= 57600){ttMark = "R";}
+    else if (totSec > 28800 && totSec < 57600){ttMark = "M";}
+    else if (totSec >= 57600){ttMark = "R";}
     var ttNum = (totSec % 28800) / 28800;
 
     var ttNumTwo = Math.round(ttNum * 100) / 100; // only two decimal digits
@@ -16,6 +19,8 @@ var TT = {
     var ttFull = ttMark + ":" + ttNum;
     var ttTwo = ttMark + ":" + ttNumTwo;
 
+    document.getElementById("seconds").innerHTML = totSec;
+    document.getElementById("mom").innerHTML = h + " " + m + " " + s;
     document.getElementById("clock").innerHTML = ttFull;
     document.getElementById("clockT").innerHTML = ttTwo;
   }
