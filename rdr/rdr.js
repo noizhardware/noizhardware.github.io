5335aa80a154e4be326adfe51cebb696c7a7710b
noizhardware <noisefuel@gmail.com>
2020-03-27 14:25:42 +0100

    Φ

--- rdr/rdr.js
+++ rdr/rdr.js
@@ -5,15 +5,9 @@ const httpGetAsync = (url, callback) => {
             callback(xmlHttp.responseText);}
     xmlHttp.open("GET", url, true); // true for asynchronous
     xmlHttp.send(null);}
-    
-
-    
-  //  hash.see()
-    
+ 
 const ipGet = (nodeId, callback) => {
      return httpGetAsync('https://raw.githubusercontent.com/noizhardware/dotfiles/master/ip/.' + nodeId, callback);}
      
 const ipGo = (nodeId) => {
      ipGet(nodeId, (ip) => locus.touch('http://' + ip));}
-
-//ipGet(hash.see(), (a)=>console.log(a));
