46f87c3024c1434f4d6fcf900439f25ac321d037
noizhardware <noisefuel@gmail.com>
2020-06-29 17:47:07 +0200

    nj

--- grain/make_grain.cmd
+++ grain/make_grain.cmd
@@ -1,20 +1,5 @@
-REM nativefier -n "floowSWH" -p windows "https://www.noizhardware.com/swh/index.html" && rename floowSWH-win32-x64 floowSWH && COPY "icons\grain.ico" "floowSWH\grain.ico" && COPY "rcedit-x64.exe" "floowSWH\rcedit-x64.exe" && rcedit-x64.exe floowSWH.exe --set-icon grain.ico && PAUSE
+nativefier --name "grain" --platform windows --icon "grain.ico" --app-copyright "noizHARDWARE" --verbose --disable-context-menu --disable-dev-tools --clear-cache --insecure --ignore-certificate index.html && cp grain_orange.png ./grain-win32-x64/grain_orange.png && cp grain.css ./grain-win32-x64/grain.css && cp grain.js ./grain-win32-x64/grain.js && cp grain.png ./grain-win32-x64/grain.png && cp index.html ./grain-win32-x64/index.html && cp nopull.css ./grain-win32-x64/nopull.css && cp nativefier.json ./grain-win32-x64/resources/app/nativefier.json && PAUSE
 
-REM https://github.com/jiahaog/nativefier/blob/master/docs/api.md
-
-REM --platform :
-REM Automatically determined based on the current OS. Can be overwritten by specifying either linux, windows, osx or mas for a Mac App Store specific build.
-REM The alternative values win32 (for Windows) or darwin, mac (for macOS) can also be used.
-
-REM -i, --icon <path>
-REM Packaging for Windows
-REM      The icon parameter should be a path to a .ico file.
-REM Packaging for Linux
-REM      The icon parameter should be a path to a .png file.
-REM Packaging for macOS
-REM      The icon parameter can either be a .icns or a .png file if the optional dependencies are installed.
-
-nativefier --name "grain" --platform windows --icon "grain.ico" --app-copyright "noizHARDWARE" --verbose --disable-context-menu --disable-dev-tools --clear-cache --insecure --ignore-certificate index.html && PAUSE
 
 REM * run the present "make_grain.cmd", it will package the app from a bogus url
 REM * Navigate to the folder that was created and copy all your site-files into the base directory (so they are at the same level as grain.exe)
