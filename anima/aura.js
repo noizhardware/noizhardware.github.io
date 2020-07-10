c39e0553d222c33e20ee93a53b5c2d99fdc72a2d
noizhardware <noisefuel@gmail.com>
2020-07-10 15:09:26 +0200

    nj

--- anima/aura.js
+++ anima/aura.js
@@ -43,20 +43,21 @@ function say (log){if(isJust(log)){console.log(log);}}
      say.alert = function(log){if(isJust(log)){console.log("::ACHTUNG ERROR:: : " + log); alert("ACHTUNG : ERROR : " + log);};};
      say.panic = function(log){if(isJust(log)){console.log('%c [[PANIC]] : ' + log, 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)'); alert("[[PANIC]] : " + log);};};
      
-function loadJS(jslocation, location, load){
-     //"location" is the location to insert the <script> element
+// function loadJS(jslocation, location, load){
+function loadJS(jslocation, fn){
+     //"location" is the location to insert the <script> element -- NOPE, ora è sempre document.head
      //"load" is the function to be executed after onload
-     if(isJust(jslocation) && isJust(location) && isJust(load)){
+     if(isJust(jslocation) && isJust(fn)){
           var scriptTag = document.createElement('script');
           scriptTag.async = true;
           scriptTag.src = jslocation;
-          scriptTag.onload = load;
-          scriptTag.onreadystatechange = load;
-          location.appendChild(scriptTag);
+          scriptTag.onload = fn;
+          scriptTag.onreadystatechange = fn;
+          document.head.appendChild(scriptTag);
           say.debug("JS loaded: " + scriptTag.src);
-          return true;
-     }else{return nil;}
-}
+          return true;}
+     else{
+          return nil;}}
 
 function zeit (){}
      zeit.start = () => {
