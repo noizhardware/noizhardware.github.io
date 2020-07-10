0269bd615733857624cd65f500a4f636fd83ee0e
noizhardware <noisefuel@gmail.com>
2020-07-10 15:21:17 +0200

    nj

--- anima/aura.js
+++ anima/aura.js
@@ -44,6 +44,7 @@ function say (log){if(isJust(log)){console.log(log);}}
      say.panic = function(log){if(isJust(log)){console.log('%c [[PANIC]] : ' + log, 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)'); alert("[[PANIC]] : " + log);};};
      
 // function loadJS(jslocation, location, load){
+// sta funzione mi sa che non funzia...
 function loadJS(jslocation, fn){
      //"location" is the location to insert the <script> element -- NOPE, ora è sempre document.head
      //"load" is the function to be executed after onload
